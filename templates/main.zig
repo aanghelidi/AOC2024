56d3008c98923d615f5f6406791974723014df09
Alain Anghelidi <alainanghelidi@gmail.com>
2024-12-02 22:11:42 +0100

    chore: update template

--- templates/main.zig
+++ templates/main.zig
@@ -6,17 +6,17 @@ const mem = std.mem;
 const print = std.debug.print;
 const testing = std.testing;
 const utils = @import("utils.zig");
+const data = @embedFile("input.txt");
 
 const Complex = math.complex.Complex;
 
 pub fn main() void {
-    const data = utils.readFile("input.txt") catch |err| {
-        print("{any}", .{err});
-        return;
-    };
     print("{s}\n", .{data});
 }
 
-test "day x part 1 2024" {}
+test "day x part 1 2024" {
+    const testing_data = @embedFile("sample.txt");
+    _ = testing_data;
+}
 
 test "day x part 2 2024" {}
