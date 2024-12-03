6c13c87884478fd7a9525a2c198a9daa6d0f2cd2
Alain Anghelidi <alainanghelidi@gmail.com>
2024-12-03 12:32:36 +0100

    feat: refactor day03

--- src/utils.zig
+++ src/utils.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const testing = std.testing;
+const ascii = std.ascii;
 
 /// On success, returns the content of the file in a []u8.
 /// On error, returns either `std.fs.Dir.StatFileError` or `std.fs.File.ReadError`.
@@ -25,8 +26,28 @@ pub fn Position(comptime T: type) type {
         }
     };
 }
+
 test "Position can init" {
     const pos = Position(u8).init(0, 0);
     try testing.expectEqual(0, pos.x);
     try testing.expectEqual(0, pos.y);
+}
+
+/// Given a []const u8 like a string returns whether or not it contains only digits
+pub fn allDigits(chars: []const u8) bool {
+    if (chars.len == 0) return false;
+    for (chars) |char| {
+        if (!ascii.isDigit(char)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+test "allDigits scenarios" {
+    try testing.expectEqual(false, allDigits("ab00"));
+    try testing.expectEqual(false, allDigits(""));
+    try testing.expectEqual(true, allDigits("0"));
+    try testing.expectEqual(true, allDigits("12349"));
+    try testing.expectEqual(false, allDigits("123.49"));
 }
