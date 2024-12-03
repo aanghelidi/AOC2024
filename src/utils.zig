dea19c8dd250f69a74491467756b8600ed743687
Alain Anghelidi <alainanghelidi@gmail.com>
2024-12-03 22:36:40 +0100

    feat: add ints utils function

--- src/utils.zig
+++ src/utils.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const testing = std.testing;
 const ascii = std.ascii;
+const fmt = std.fmt;
+const assert = std.debug.assert;
+const math = std.math;
 
 /// On success, returns the content of the file in a []u8.
 /// On error, returns either `std.fs.Dir.StatFileError` or `std.fs.File.ReadError`.
@@ -11,7 +14,7 @@ pub fn readFile(filename: []const u8) ![]u8 {
     return data;
 }
 
-/// Simple generic struct containing Position info
+/// Simple generic struct containing Position info.
 pub fn Position(comptime T: type) type {
     return struct {
         const Self = @This();
@@ -27,13 +30,7 @@ pub fn Position(comptime T: type) type {
     };
 }
 
-test "Position can init" {
-    const pos = Position(u8).init(0, 0);
-    try testing.expectEqual(0, pos.x);
-    try testing.expectEqual(0, pos.y);
-}
-
-/// Given a []const u8 like a string returns whether or not it contains only digits
+/// Given a []const u8 like a string returns whether or not it contains only digits.
 pub fn allDigits(chars: []const u8) bool {
     if (chars.len == 0) return false;
     for (chars) |char| {
@@ -44,10 +41,82 @@ pub fn allDigits(chars: []const u8) bool {
     return true;
 }
 
+/// Parses a string and extracts all integers, including negative numbers.
+///
+/// This function scans the input string for sequences of digits, optionally preceded by a '-' sign,
+/// and converts them into integers of type `T`. The integers are collected into an `ArrayList(T)`.
+/// Returns an compile time error when `T` is not a signed integers.
+pub fn ints(comptime T: type, text: []const u8, allocator: std.mem.Allocator) !std.ArrayList(T) {
+    if (@typeInfo(T) != .int or @typeInfo(T).int.signedness != .signed) {
+        @compileError("`a` and `b` must be signed integers");
+    }
+
+    var nums = std.ArrayList(T).init(allocator);
+
+    var i: usize = 0;
+    while (i < text.len) : (i += 1) {
+        if (!ascii.isDigit(text[i]) and text[i] != '-') continue;
+        var is_negative = false;
+        if (text[i] == '-') {
+            is_negative = true;
+            i += 1;
+            if (i >= text.len or !ascii.isDigit(text[i])) continue;
+        }
+        const start = i;
+        while (i < text.len and ascii.isDigit(text[i])) : (i += 1) {}
+        const num_str = text[start..i];
+        const num = try std.fmt.parseInt(T, num_str, 10);
+        try nums.append(if (is_negative) -num else num);
+    }
+
+    return nums;
+}
+
+test "Position can init" {
+    const pos = Position(u8).init(0, 0);
+    try testing.expectEqual(0, pos.x);
+    try testing.expectEqual(0, pos.y);
+}
+
 test "allDigits scenarios" {
     try testing.expectEqual(false, allDigits("ab00"));
     try testing.expectEqual(false, allDigits(""));
     try testing.expectEqual(true, allDigits("0"));
     try testing.expectEqual(true, allDigits("12349"));
     try testing.expectEqual(false, allDigits("123.49"));
+}
+
+test "parsing ints" {
+    var result = try ints(i16, "Here are some numbers: -42, 123, -567, 890, and -12.", testing.allocator);
+    defer result.deinit();
+    try std.testing.expectEqualSlices(i16, &[_]i16{ -42, 123, -567, 890, -12 }, result.items);
+
+    var result_2 = try ints(i8, "1 2 3 4 5", testing.allocator);
+    defer result_2.deinit();
+    try std.testing.expectEqualSlices(i8, &[_]i8{ 1, 2, 3, 4, 5 }, result_2.items);
+
+    var result_3 = try ints(i8, "-1 -2 -3 -4 -5", testing.allocator);
+    defer result_3.deinit();
+    try std.testing.expectEqualSlices(i8, &[_]i8{ -1, -2, -3, -4, -5 }, result_3.items);
+
+    var result_4 = try ints(i8, "No numbers here!", testing.allocator);
+    defer result_4.deinit();
+    try std.testing.expectEqualSlices(i8, &[_]i8{}, result_4.items);
+
+    var result_5 = try ints(i32, "1234567890", testing.allocator);
+    defer result_5.deinit();
+    try std.testing.expectEqualSlices(i32, &[_]i32{1234567890}, result_5.items);
+
+    var result_6 = try ints(i16, "  123  456  789  ", testing.allocator);
+    defer result_6.deinit();
+    try std.testing.expectEqualSlices(i16, &[_]i16{ 123, 456, 789 }, result_6.items);
+
+    var result_7 = try ints(i16, "123abc-456def789", testing.allocator);
+    defer result_7.deinit();
+    try std.testing.expectEqualSlices(i16, &[_]i16{ 123, -456, 789 }, result_7.items);
+
+    // Should raise a compile time error
+    // comptime {
+    //     _ = ints(u32, "123", std.testing.allocator);
+    // }
 }
